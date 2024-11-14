048e27805069424a79bca7b95eef9ae3bba6c05b
Kenton Troy <kentontroy@yahoo.com>
2024-11-14 18:02:53 -0500

    Update create-relationship-customer-order.go

--- src/neptune-database-load/go/create-relationship-customer-order.go
+++ src/neptune-database-load/go/create-relationship-customer-order.go
@@ -68,24 +68,43 @@ func main() {
 	// Parse the date using a specifc example format "2006-01-02 15:04:05"
 	// Note a generic "yyyy-mm-dd hh:mm:ss" cannot be used
 	// Format to ISO 8601 "2006-01-02T15:04:05Z"
+	// But the current version of openCypher in use only supports datetime() with
+	// static string literals, not with dynamic properties. This is a known limitation
+	// in some openCypher implementations, including AWS Neptune's partial support for openCypher.
+	// To enable datetime queries, decompose the year, month, and day into separate properties
 	dateValues := make([]string, numRows)
+	yearValues := make([]int, numRows)
+	monthValues := make([]int, numRows)
+	dayValues := make([]int, numRows)
 	for i, val := range newDf.Col("order_purchase_timestamp:String").Records() {
 		parsedTime, err := time.Parse("2006-01-02 15:04:05", val)
 		if err != nil {
 			log.Fatalf("Failed to parse date: %v", err)
 		}
-		//dateValues = append(dateValues, parsedTime.Format(time.RFC3339))
 		dateValues[i] = parsedTime.Format(time.RFC3339)
+		monthValues[i] = int(parsedTime.Month())
+		dayValues[i] = parsedTime.Day()
+		yearValues[i] = parsedTime.Year()
 	}
-	dateSeries := series.New(dateValues, series.String, "order_purchase_timestamp:Date")
+	dateSeries := series.New(dateValues, series.String, "order_purchase_timestamp")
+	monthSeries := series.New(monthValues, series.Int, "order_purchase_timestamp_month:Int")
+	daySeries := series.New(dayValues, series.Int, "order_purchase_timestamp_day:Int")
+	yearSeries := series.New(yearValues, series.Int, "order_purchase_timestamp_year:Int")
+
 	newDf = newDf.Mutate(dateSeries)
+	newDf = newDf.Mutate(monthSeries)
+	newDf = newDf.Mutate(daySeries)
+	newDf = newDf.Mutate(yearSeries)
 
 	newDf = newDf.Select([]string{
 		":ID",
 		":START_ID(customer)",
 		":END_ID(order)",
 		":TYPE",
-		"order_purchase_timestamp:Date",
+		"order_purchase_timestamp",
+		"order_purchase_timestamp_month:Int",
+		"order_purchase_timestamp_day:Int",
+		"order_purchase_timestamp_year:Int",
 	})
 
 	fmt.Println(newDf)
