6d205110e789c13df46b48369316deb92cf12f36
Kenton Troy <kentontroy@yahoo.com>
2024-11-17 17:48:06 -0500

    Update load-to-neptune.sh

--- scripts/load-to-neptune.sh
+++ scripts/load-to-neptune.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-export AWS_REGION="us-east-2"
-export AWS_NEPTUNE_LOADER_FORMAT="opencypher"
-export AWS_NEPTUNE_ENDPOINT="db-neptune-aws-reinvent.cluster-cmabrddbmjfm.us-east-2.neptune.amazonaws.com"
-export AWS_NEPTUNE_ENDPOINT_PORT=8182
-export AWS_NEPTUNE_ARN="arn:aws:iam::317913635185:role/NeptuneLoadFromS3"
-
 # Check if bucket name and key are provided
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <bucket-name> <bucket-key>"
@@ -20,7 +14,7 @@ AWS_S3_BUCKET_KEY=$2
 # References
 # https://docs.aws.amazon.com/neptune/latest/userguide/load-api-reference-load-examples.html
 ##############################################################################################
-load_file="s3://${AWS_S3_BUCKET}/${AWS_S3_BUCKET_KEY}"
+load_file="s3://$1/$2"
 echo "$load_file"
 
 curl -X POST \
