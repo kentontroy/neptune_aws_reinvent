8c8f6c0cd994851b11fd0ce608e70f93d762db43
Kenton Troy <kentontroy@yahoo.com>
2024-11-17 20:09:04 -0500

    Update initiate_clear_graph.sh

--- scripts/initiate_clear_graph.sh
+++ scripts/initiate_clear_graph.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export AWS_NEPTUNE_ENDPOINT="db-neptune-aws-reinvent.cluster-cmabrddbmjfm.us-east-2.neptune.amazonaws.com"
-export AWS_NEPTUNE_ENDPOINT_PORT=8182
-
 ##############################################################################################
 # References
 # https://docs.aws.amazon.com/neptune/latest/userguide/manage-console-fast-reset.html#:~:text=To%20delete%20all%20data%20from%20a%20Neptune%20DB%20cluster%20using%20the%20API&text=You%20do%20this%20by%20sending,to%20specify%20the%20initiateDatabaseReset%20action.&text=The%20token%20remains%20valid%20for,minutes)%20after%20it%20is%20issued.
